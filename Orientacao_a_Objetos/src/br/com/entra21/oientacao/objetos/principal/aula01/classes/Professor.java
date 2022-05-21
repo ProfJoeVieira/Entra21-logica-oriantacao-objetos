ceca57cc4fdcac330f5ea7780b16e7d0ecc64675
ProfJoeVieira <profjoevieirajr@gmail.com>
2022-05-21 09:00:47 -0300

    asAssuntos aula 1 - orientação a objetos

---
+++ Orientacao_a_Objetos/src/br/com/entra21/oientacao/objetos/principal/aula01/classes/Professor.java
@@ -0,0 +1,40 @@
+package br.com.entra21.oientacao.objetos.principal.aula01.classes;
+
+public class Professor {
+
+ public static String instituicao;
+//Atributos
+ private 	String nome;
+ private 	byte idade;
+ private 	byte quantidadeAlunos;
+ 
+ public Professor() {
+	 //Construtor vazio não inicializa os atributos
+ }
+ 
+ public Professor(String nome, byte idade) {
+	 this.nome=nome;
+	 this.idade=idade;
+	 //Construtor com parametros
+	 //Prode inicializar alguns ou todos os 
+	 
+	 
+ }
+
+public String getNome() {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+public void setNome(String string) {
+	// TODO Auto-generated method stub
+	
+}
+
+public String getIdade() {
+	// TODO Auto-generated method stub
+	return null;
+}
+	
+}
+
